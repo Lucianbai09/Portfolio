37a27a5ccfb016b3aa66383cf571226333ad878d
KingBobSkibidi <lucianbai0808@gmail.com>
2026-05-24 18:08:01 -0400

    May 24: starting outline
    - created basic outline for project
    - added mini database python file, folders including template and styling, css styling file, base html file and home page html file, readme, license, and gitignore

---
+++ data.py
@@ -0,0 +1,23 @@
+projects = [
+    {
+        "name": "Sample Project",
+        "description": "This is a placeholder project.",
+        "tech": ["Python", "Flask"]
+    }
+]
+
+achievements = [
+    "Joined FBLA",
+    "Completed coding competition"
+]
+
+hobbies = [
+    "Ultimate Frisbee",
+    "Gym",
+    "Coding"
+]
+
+favorites = {
+    "music": ["Artist 1", "Artist 2"],
+    "food": ["Pizza", "Sushi"]
+}
