eddd29fb4027090756130b162f04d0bd4fbeae32
KingBobSkibidi <lucianbai0808@gmail.com>
2026-05-26 23:35:05 -0400

    May 26: styling and html tweaks
    - deleted JavaScript run page when ready
    - tweaked styling and html pages
    - tweaked html file indenting
    - tweaked some info

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,3 @@
-// run when page loads
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Portfolio loaded");
-});
-
 // toggle light/dark theme
 function toggleTheme() {
     document.body.classList.toggle("dark");
