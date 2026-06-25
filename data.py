081b9d3ec715f838a680474a48b5c7ab31d12164
KingBobSkibidi <lucianbai0808@gmail.com>
2026-06-24 23:47:22 -0500

    June 24: timeline entries + bug fix
    - added and edited more timeline entries
    - updated description of timeline cards to at most 3 lines
    - fixed sideline/tabs bar bug, item used to be at top of page for corresponding sideline tab, caused bug where you couldn't click some items, now centered

--- data.py
+++ data.py
@@ -8,18 +8,50 @@ timeline = [
         "link": "https://programming-24.mooc.fi/"
     },
     {
+        "year": "2024-2025",
+        "title": "Music Council Grade Rep",
+        "category": "Experience",
+        "description": "Served as grade representative on Trudeau Music's music council.",
+        "image": "mooc2024.webp",
+        "link": "https://trudeaumusic.wixsite.com/peths"
+    },
+    {
+        "year": "2024",
+        "title": "DECA Regional Champion",
+        "category": "Competition",
+        "description": "Top 10 roleplay, Top 10 MC, Top 3 overall at DECA Regionals.",
+        "image": "mooc2024.webp",
+        "link": "https://deca.ca/v2/about-regionals/"
+    },
+    {
+        "year": "2025",
+        "title": "Autotyper",
+        "category": "Project",
+        "description": "My first coding project, a GUI tool that types out pasted text with custom delays and breaks. Built with Python and Tkinter.",
+        "image": "mooc2024.webp",
+        "link": "https://github.com/KingBobSkibidi/autotyper"
+    },
+    {
         "year": "2025",
         "title": "Pedalheads Bike Instructor",
         "category": "Experience",
-        "description": "",
+        "description": "Summer bike camp instructor, taught 50+ children ages 4-10 how to ride bikes with over 150+ hours of instruction.",
         "image": "pedalheads.webp",
-        "link": "https://www.instagram.com/berczy_ultimate?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+        "link": "https://pedalheads.com/en/bike?utm_term=pedalheads&utm_campaign=G_Search_Branded_Bike_CAN_ON_All&utm_source=google&utm_medium=cpc&hsa_acc=3891153763&hsa_cam=23502517136&hsa_grp=190593523605&hsa_ad=794630995400&hsa_src=g&hsa_tgt=kwd-491131322832&hsa_kw=pedalheads&hsa_mt=b&hsa_net=adwords&hsa_ver=3&gad_source=1&gad_campaignid=23502517136&gbraid=0AAAAADnnjxyemrKAkZR98UibY4vEAdsRU&gclid=CjwKCAjwgO7RBhBKEiwAZNP85oWj7Jw5PAvQEhWpSljYDyFy2osv0sWtNJoIy7H_hmQyeXJqO89XuxoCwQ4QAvD_BwE"
+    },
+    {
+        "year": "2025",
+        "title": "Webtoon Tracker",
+        "category": "Project",
+        "description": "My first web app, a Webtoon chapter and status tracking platform. Built with Flask (Jinja2), SQLite, HTML, and CSS.",
+        "image": "mooc2024.webp",
+        "link": "https://github.com/KingBobSkibidi/Webtoon-App"
     },
     {
         "year": "2025",
         "title": "Go-Kart Couch",
         "category": "Project",
-        "description": "",
+        "description": "First physical project, built a motorized couch go-kart, combination of physical engineering and programming.",
         "image": "mooc2024.webp",
         "link": "https://www.instagram.com/berczy_ultimate?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
     },
@@ -27,16 +59,23 @@ timeline = [
         "year": "2025-present",
         "title": "Founded Berczy Ultimate",
         "category": "Experience",
-        "description": """Co-founded Berczy Ultimate, a local youth ultimate frisbee club hosting weekly drop in meetings
-        promoting an inclusive and diverse environment. Sponsered by Ultimate Canada.""",
+        "description": "Co-founded Berczy Ultimate. Sponsered by Ultimate Canada and the WFDF.",
         "image": "mooc2024.webp",
         "link": "https://www.instagram.com/berczy_ultimate?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+    },
+    {
+        "year": "2025-2026",
+        "title": "Robotics Provincial Top 16",
+        "category": "Achievement",
+        "description": "Lead coder (C++) and builder for Trubotics, my school's VEX V5 team, #1 public school in Ontario at provincials",
+        "image": "mooc2024.webp",
+        "link": "https://github.com/KingBobSkibidi/Robotics-2025-2026"
     },
     {
         "year": "2026",
         "title": "FBLC: Coding and Programming",
         "category": "Competition",
-        "description": "Placed 5th nationally in team competition with 'Locally' project as a solo participant",
+        "description": "Placed 5th nationally in team competition with 'Locally' project as a solo participant.",
         "image": "fblc.webp",
         "link": "https://fblc.ca/coding-and-programming"
     },
@@ -44,8 +83,7 @@ timeline = [
         "year": "2026",
         "title": "Locally",
         "category": "Project",
-        "description": """Web application that helps users discover and support small local 
-        businesses. Built with Flask (Jinja2), PostgreSQL, HTML, and CSS.""",
+        "description": "Web app to help users discover and support local businesses. Built with Flask (Jinja2), PostgreSQL, HTML, and CSS.",
         "image": "locally.webp",
         "link": "https://fblc-business-project-fwjvzk3wl-lucian-bais-projects.vercel.app/"
     },
@@ -53,9 +91,17 @@ timeline = [
         "year": "2026",
         "title": "Young Sparks STEM Workshop",
         "category": "Experience",
-        "description": "",
+        "description": "Hosted a hands on STEM workshop for 25+ children in collaboration with Minds In Motion STEM INC.",
         "image": "workshop.webp",
         "link": "https://www.instagram.com/berczy_ultimate?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+    },
+    {
+        "year": "2026",
+        "title": "Varsity Frisbee Team",
+        "category": "Experience",
+        "description": "Member of Pierre Elliott Trudeau High School's Varsity Ultimate Frisbee Team.",
+        "image": "mooc2024.webp",
+        "link": "https://www.instagram.com/p/DZq9zrrlslc/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
     },
     {
         "year": "2026-present",
